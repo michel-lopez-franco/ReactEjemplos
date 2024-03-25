0eaf3c6762321728581e300d2010aed6f2e1ff2d
michel-lopez-franco <michel.lopez@academicos.udg.mx>
2024-03-25 02:01:45 -0600

    Ejemplo creando varios botones

--- 00-hola-mundo/src/main.jsx
+++ 00-hola-mundo/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.Fragment>
+      <button>Hola button 1</button>
+      <button>Hola button 2</button>
+      <button>Hola button 3</button>
+      <button>Hola button 4</button>
+    </React.Fragment>
 )
