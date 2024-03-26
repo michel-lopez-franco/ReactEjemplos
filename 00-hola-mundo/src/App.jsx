fac1d2f3c57121784ca443a61a04204525e22e5d
michel-lopez-franco <michel.lopez@academicos.udg.mx>
2024-03-25 20:43:50 -0600

    Añadiendo eventos a los botones pero no me convence la forma

--- 00-hola-mundo/src/App.jsx
+++ 00-hola-mundo/src/App.jsx
@@ -1,30 +1,52 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard';
+
 
 export function App() {
-    return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img
-                className='tw-followCard-avatar' 
-                src="https://mighty.tools/mockmind-api/content/human/49.jpg" 
-                alt="Michel logo" 
-                />
-                <div className='tw-followCard-info'>
-                    <strong className='tw-followCard-infoName'>Human 49 </strong>
-                    <span className='tw-followCard-infoUserName'>@Human 49</span>
-                </div>
-            </header>
+    const format = (userName) =>`@${userName}`; 
 
-            <aside className='tw-followCard-aside'>
-                <button className='tw-followCard-button'>
-                    Follow
-                </button>
-            </aside>
-        </article>
+    return (
+        <section className='App'>
+        
+        <TwitterFollowCard
+            formatUserName= {format} 
+            name= {'Human 49'} 
+            userName={'Human 49'} 
+            url={'https://mighty.tools/mockmind-api/content/human/49.jpg'}
+            isFollowing = {false}
+        />
+
+        <TwitterFollowCard
+            formatUserName= {format}             
+            name= {'Human 43'} 
+            userName={'Human 43'} 
+            url={'https://mighty.tools/mockmind-api/content/human/43.jpg'}
+            isFollowing 
+        />        
+
+        <TwitterFollowCard 
+            formatUserName= {format} 
+            name= {'Human 68'} 
+            userName={'Human 68'} 
+            url={'https://mighty.tools/mockmind-api/content/human/68.jpg'}
+            isFollowing={true}
+        /> 
+        <TwitterFollowCard
+            formatUserName= {format} 
+            name= {'Human a Human 28'} 
+            userName={'Human 28'} 
+            url={'https://mighty.tools/mockmind-api/content/human/28.jpg'}
+        />
+
+
+        </section>
     );
 }
 
 
-
+//Human 49
+//@Human 49
+//https://mighty.tools/mockmind-api/content/human/49.jpg
 
 //https://mighty.tools/mockmind-api/content/human/43.jpg
+//https://uifaces.co/
---
+++ 00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,53 @@
+
+
+export function TwitterFollowCard({formatUserName, userName, name, url, isFollowing}) {
+    
+    //const addAt = (userName) =>`@${userName}`; 
+    
+    function handleFollowClick(e) {
+        alert(`¡Reproduciendo ${JSON.stringify(e) }!`);
+    }
+
+    const isFollow = (isFollowing) => {
+        
+        if (isFollowing){
+        
+            return (
+            <button className='tw-followCard-button' onClick={handleFollowClick}>
+            Follow
+            </button>)
+        } else {
+            return (
+            <button className='tw-followCard-button' onClick={handleFollowClick}>
+            Unfollow
+            </button>
+            )
+        }
+
+    }
+
+    
+
+    return (
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img
+                className='tw-followCard-avatar' 
+                src= {url} 
+                alt="Michel logo" 
+                />
+                <div className='tw-followCard-info'>
+                    <strong className='tw-followCard-infoName'>{name} </strong>
+                    <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
+                </div>
+            </header>
+            
+            <aside className='tw-followCard-aside'>
+                
+            {isFollow(isFollowing)}
+                
+            </aside>
+        </article>
+    );
+}
+//https://mighty.tools/mockmind-api/content/human/49.jpg
